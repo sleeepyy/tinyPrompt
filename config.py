2f96da5dd1623ff9cc95fe6ac5fe877e1aa7b4d3
iyoghurts <yangha9826@gmail.com>
2017-04-14 23:02:29 +0800

    push

--- config.py
+++
@@ -1,9 +0,0 @@
-city = "hangzhou"
-weather_url = "http://weather.sina.com.cn/" + city
-
-sender = "qingacfun@163.com"
-receivers = ["994819188@qq.com"]
-
-host = "smtp.163.com"
-user = "18867102166@163.com"
-password = "qingac98"
