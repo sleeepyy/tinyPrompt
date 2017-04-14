b5107d5baec84c2b8e8a4e0f709bac8ef812d49d
sleeepyy <yanghan9826@gmail.com>
2017-04-14 23:38:22 +0800

    push

---
+++ config.py
@@ -0,0 +1,8 @@
+sender = "qingacfun@163.com"
+receivers = ["994819188@qq.com"]
+
+host = "https://smtp.163.com"
+
+user = "qingacfun@163.com"
+password = "yanghan98"
+weather_url = "http://weather.sina.com.cn/"
