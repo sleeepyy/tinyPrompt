3a1ae0c9a850d3494dcb10970c8b9c94f20a28ad
iyoghurts <yangha9826@gmail.com>
2017-04-15 09:51:21 +0800

    modify

--- config.py
+++
@@ -1,8 +0,0 @@
-sender = "qingacfun@163.com"
-receivers = {"hangzhou":["994819188@qq.com"], "shanghai":["570461412@qq.com", "994819188@qq.com"]}
-
-host = "smtp.163.com"
-
-user = "qingacfun"
-password = "yanghan123"
-weather_url = "http://weather.sina.com.cn/"
